0280347ae561c7b96a9cb0b7d84caa65972eb29a
Mario Rossi <mario@example.com>
2026-09-23 15:15:56 +0200

    Test Elimna e ricrea file stesso nome diverso autore

---
+++ Hanami Javascript File/Hanami Test.js
@@ -0,0 +1,11 @@
+/*
+ * --------------------------------------------------------------------------
+ *
+ * Author:      Mario
+ *
+ * website: https://www.hanamisoftware.com
+ * License:     MIT
+ * Copyright (c) 2026 Hanami Software
+ * --------------------------------------------------------------------------
+ */
+// mi aspetto Mario come Autore di questo file.
